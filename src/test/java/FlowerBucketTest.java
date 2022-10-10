ee8dff01f4269e0a6480bc1892482b81441fc38e
yurii-zinchuk <yurii.zinchuk@ucu.edu.ua>
2022-10-10 12:34:55 +0300

    added tests

---
+++ src/test/java/FlowerBucketTest.java
@@ -0,0 +1,26 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import shop.Flower;
+import shop.Rose;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FlowerBucketTest {
+    private FlowerBucket flowerBucket;
+
+    @BeforeEach
+    public void init() {
+        Flower flower = new Rose();
+        flower.setPrice(10);
+        FlowerPack flowerPack = new FlowerPack(flower, 10);
+        flowerBucket = new FlowerBucket();
+        flowerBucket.add(flowerPack);
+    }
+
+    @Test
+    private void testPrice() {
+        assertEquals(100, flowerBucket.getPrice());
+    }
+
+}
---
+++ src/test/java/FlowerTest.java
@@ -0,0 +1,30 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import shop.Flower;
+import shop.FlowerColor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FlowerTest {
+    private Flower flower;
+
+    @BeforeEach
+    public void init() {
+        flower = new Flower();
+    }
+
+    @Test
+    private void testPrice() {
+        int price = 20;
+        flower.setPrice(price);
+        assertEquals(price, flower.getPrice());
+    }
+
+    @Test
+    private void testColor() {
+        FlowerColor price = FlowerColor.RED;
+        flower.setColor(price);
+        assertEquals("#FF0000", flower.getColor());
+    }
+}
